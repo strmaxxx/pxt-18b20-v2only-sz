ec12d111769e3f69fb61ae3c63ec56f6e56ffc79
Lionkk <Lion_yan@outlook.com>
2020-12-28 17:11:18 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,64 @@
-basic.forever(function () {
-	
-})
+//% color=#5042f4 icon="\uf2c9"
+namespace DS18B20{
+    let sc_byte = 0
+    let dat = 0
+    let low = 0
+    let high = 0
+    let temp = 0
+    let temperature = 0
+    let ack = 0
+    function init_18b20(mpin:DigitalPin) {
+        pins.digitalWritePin(mpin, 0)
+        control.waitMicros(600)
+        pins.digitalWritePin(mpin, 1)
+        control.waitMicros(30)
+        ack = pins.digitalReadPin(mpin)
+        control.waitMicros(600)
+        return ack
+    }
+    function write_18b20 (mpin:DigitalPin,data: number) {
+        sc_byte = 0x01
+        for (let index = 0; index < 8; index++) {
+            pins.digitalWritePin(mpin, 0)
+            if (data & sc_byte) {
+                pins.digitalWritePin(mpin, 1)
+                control.waitMicros(60)
+            } else {
+                pins.digitalWritePin(mpin, 0)
+                control.waitMicros(60)
+            }
+            pins.digitalWritePin(mpin, 1)
+            data = data >> 1
+        }
+    }
+    function read_18b20 (mpin:DigitalPin) {
+        dat = 0x00
+        sc_byte = 0x01
+        for (let index = 0; index < 8; index++) {
+            pins.digitalWritePin(mpin, 0)
+            pins.digitalWritePin(mpin, 1)
+            if (pins.digitalReadPin(mpin)) {
+                dat = dat + sc_byte
+            }
+            sc_byte = sc_byte << 1
+            control.waitMicros(60)
+        }
+        return dat
+    }
+    //% block="value of DS18B20 at pin %pin"
+    export function Ds18b20Temp(pin:DigitalPin):number{
+        init_18b20(pin)
+        write_18b20(pin,0xCC)
+        write_18b20(pin,0x44)
+        basic.pause(10)
+        init_18b20(pin)
+        write_18b20(pin,0xCC)
+        write_18b20(pin,0xBE)
+        low = read_18b20(pin)
+        high = read_18b20(pin)
+        temperature = high << 8 | low
+        temperature = temperature * 16
+        return temperature
+    }
+
+}
